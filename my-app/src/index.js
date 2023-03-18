101b7fde985efc17254fc912ea1870e86bd92265
JadenJT <jaden@LAPTOP-VJU7OPRD.localdomain>
2023-03-18 15:50:03 -0500

    got rid of stuff and started making login

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Blogs from './pages/blogs';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./pages/home";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+  <BrowserRouter>
+  <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="blogs" element={<Blogs />} />
+    </Route>
+  </Routes>
+</BrowserRouter>
+   
+ 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ my-app/src/pages/blogs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { BrowserRouter as router, Switch, Route} from 'react-router-dom';
+const Blogs = () => {
+  return (
+    <h1>You can write your blogs!</h1>
+  );
+};
+  
+export default Blogs;
