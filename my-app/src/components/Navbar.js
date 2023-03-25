385e17f3c9143c7f194b65ce951f7c08672de557
JadenJT <jaden@LAPTOP-VJU7OPRD.localdomain>
2023-03-25 14:11:17 -0500

    changed some styles

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,29 +1,29 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import "./Navbar.css";
+
 function Navbar() {
-  const[click, setClick] = useState(false);
-  const handleClick = () => setClick(!click);
   return (
   
  <nav className="navbar">
   <div className="navbar-container">
     <Link to='/' className='navbar-logo'>
-      Home
+      <a class="cursor-pointer px-1 text-5xl font-medium text-off-white hover:underline md:text-2xl">Home</a>
     </Link>
     <div className='register'>
       <Link to='/register' className='navbar-logo'>
       Register
       </Link>
     </div>
-    <div className='Login'>
-      <Link to='/Login' className='navbar-logo'>
-      Login
-      </Link>
-    </div>
     <div className='Help'>
       <Link to='/help' className='navbar-logo'>
       Help
+      </Link>
+    </div>
+
+    <div className='Login'>
+      <Link to='/Login' className='navbar-logo'>
+      Login
       </Link>
     </div>
      
