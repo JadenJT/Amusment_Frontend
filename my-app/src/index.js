a3fc96b923fc6ee9464bc695f7e69e04ebbde4bc
MiguelSal <123981962+MiguelSal@users.noreply.github.com>
2023-03-26 00:44:48 -0500

    Quick edit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,15 +6,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./pages/home";
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <BrowserRouter>
   <Routes>
       <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} /> /**/
+      <Route index element={<Home />} />
       <Route path="blogs" element={<Blogs />} />
     </Route>
   </Routes>
