61b21ba707275c20af352e0ce92adbb287c51b41
MiguelSal <123981962+MiguelSal@users.noreply.github.com>
2023-03-27 00:54:39 -0500

    Deals white logo and pictures

--- my-app/src/pages/zone/zone.js
+++ my-app/src/pages/zone/zone.js
@@ -1,23 +1,41 @@
 import React from "react";
+import Logo from '../icons/Umazing.svg';
 import "./zone.css"
-import whiteLogo from '../../images/whitelogo.png'
-import Popcorn from '../../images/concessions/popcorn.jpg'
-import CottonCandy from '../../images/concessions/cottoncandy.jpg'
-import Lemonade from '../../images/concessions/lemonade.jpg'
-import GiftShop1 from '../../images/giftshop/giftshop1.jpg'
-import FunnerlCake from '../../images/concessions/funnelcake.jpg'
-import IceCream from '../../images/concessions/icecream.jpg'
-import GiftShop2 from '../../images/giftshop/giftshop2.jpg'
-import Burger from '../../images/concessions/burger.jpg'
-import Turkey from '../../images/concessions/turkey.jpg'
-import Hotdog from '../../images/concessions/hotdogs.jpg'
-import GiftShop3 from '../../images/giftshop/giftshop3.jpg'
+import Popcorn from '../images/concessions/popcorn.jpg'
+import CottonCandy from '../images/concessions/cottoncandy.jpg'
+import Lemonade from '../images/concessions/lemonade.jpg'
+import GiftShop1 from '../images/giftshop/giftshop1.jpg'
+import FunnerlCake from '../images/concessions/funnelcake.jpg'
+import IceCream from '../images/concessions/icecream.jpg'
+import GiftShop2 from '../images/giftshop/giftshop2.jpg'
+import Burger from '../images/concessions/burger.jpg'
+import Turkey from '../images/concessions/turkey.jpg'
+import Hotdog from '../images/concessions/hotdogs.jpg'
+import GiftShop3 from '../images/giftshop/giftshop3.jpg'
 
-export default function HomePage(){
+import SkyBlade from '../images/SkyBlade.jpg'
+import VortexX from '../images/VortexX.jpg'
+import Thunderbolt from '../images/Thunderbolt.jpg'
+import RainbowSwirl from '../images/RainbowSwirl.jpg'
+import JungleSafari from '../images/JungleSafari.jpg'
+import GForce from '../images/G-Force.jpg'
+import WarpZone from '../images/WarpZone.webp'
+import Wildfire from '../images/Wildfire.jpg'
+import PiratePlunge from '../images/PiratePlunge.jpg'
+import SpaceExplorer from '../images/SpaceExplorer.webp'
+import AeroJet from '../images/AeroJet.jpg'
+import CosmicCruiser from '../images/CosmicCruiser.jpg'
+import Inferno from '../images/Inferno.jpg'
+import DragonsLair from '../images/DragonsLair.jpg'
+import DinoAdventure from '../images/DinoAdventure.jpg'
+
+
+export default function Zone(){
+    console.log("poop");
     return (
         <>
             <div class="zwp">
-                <img src={whiteLogo} alt="park logo" class="wLogo"></img>
+                <img src={Logo} alt="park logo" class="wLogo"></img>
             </div>
 
             <div class="zone1">
@@ -25,6 +43,7 @@ export default function HomePage(){
                 <div class="column">
                     <div class="card">                    
                         <ul>
+                            <li><img class="view" src={SkyBlade} alt="SkyBlade"></img></li>
                             <li><h3>SkyBlade</h3></li>
                             <li>Type: Rollercoaster</li>
                             <li>Location: Zone1</li>
@@ -35,6 +54,7 @@ export default function HomePage(){
                 <div class="column">
                     <div class="card">                    
                         <ul>
+                            <li><img class="view" src={VortexX} alt="VortexX"></img></li>
                             <li><h3>VortexX</h3></li>
                             <li>Type: Rollercoaster</li>
                             <li>Location: Zone1</li>
@@ -45,6 +65,7 @@ export default function HomePage(){
                 <div class="column">
                     <div class="card">                    
                         <ul>
+                            <li><img class="view" src={Thunderbolt} alt="Thunderbolt"></img></li>
                             <li><h3>Thunderbolt</h3></li>
                             <li>Type: Rollercoaster</li>
                             <li>Location: Zone1</li>
@@ -55,6 +76,7 @@ export default function HomePage(){
                 <div class="column">
                     <div class="card">                    
                         <ul>
+                            <li><img class="view" src={RainbowSwirl} alt="Rainbow Swirl"></img></li>
                             <li><h3>Rainbow Swirl</h3></li>
                             <li>Type: Rollercoaster</li>
                             <li>Location: Zone1</li>
@@ -65,6 +87,7 @@ export default function HomePage(){
                 <div class="column">
                     <div class="card">                    
                         <ul>
+                            <li><img class="view" src={JungleSafari} alt="Jungle Safari"></img></li>
                             <li><h3>Jungle Safari</h3></li>
                             <li>Type: Rollercoaster</li>
                             <li>Location: Zone1</li>
@@ -75,7 +98,7 @@ export default function HomePage(){
                 <div class="column">
                     <div class="card">                    
                         <ul>
-                            <li ><img class="view" src={Popcorn} alt="popcorn"></img></li>
+                            <li><img class="view" src={Popcorn} alt="popcorn"></img></li>
                             <li><h3>Popcorn</h3></li>
                             <li>Type: Concession</li>
                             <li>Location: Zone1</li>
@@ -123,6 +146,7 @@ export default function HomePage(){
                 <div class="column">
                     <div class="card">                    
                         <ul>
+                            <li><img class="view" src={GForce} alt="G Force"></img></li>
                             <li><h3>G-Force</h3></li>
                             <li>Type: Rollercoaster</li>
                             <li>Location: Zone2</li>
@@ -133,6 +157,7 @@ export default function HomePage(){
                 <div class="column">
                     <div class="card">                    
                         <ul>
+                            <li><img class="view" src={WarpZone} alt="Warp Zone"></img></li>
                             <li><h3>WarpZone</h3></li>
                             <li>Type: Rollercoaster</li>
                             <li>Location: Zone2</li>
@@ -143,6 +168,7 @@ export default function HomePage(){
                 <div class="column">
                     <div class="card">                    
                         <ul>
+                            <li><img class="view" src={Wildfire} alt="Wildfire"></img></li>
                             <li><h3>Wildfire</h3></li>
                             <li>Type: Rollercoaster</li>
                             <li>Location: Zone2</li>
@@ -153,6 +179,7 @@ export default function HomePage(){
                 <div class="column">
                     <div class="card">                    
                         <ul>
+                            <li><img class="view" src={PiratePlunge} alt="Pirate Plunde"></img></li>
                             <li><h3>Pirate Plunge</h3></li>
                             <li>Type: Rollercoaster</li>
                             <li>Location: Zone2</li>
@@ -163,6 +190,7 @@ export default function HomePage(){
                 <div class="column">
                     <div class="card">                    
                         <ul>
+                            <li><img class="view" src={SpaceExplorer} alt="Space Explorer"></img></li>
                             <li><h3>Space Explorer</h3></li>
                             <li>Type: Rollercoaster</li>
                             <li>Location: Zone2</li>
@@ -209,7 +237,7 @@ export default function HomePage(){
                 <h1 class="zName">Zone3</h1>
                 <div class="column">
                     <div class="card">
-                        <ul>
+                        <ul><li><img class="view" src={AeroJet} alt="AeroJet"></img></li>
                             <li><h3>AeroJet</h3></li>
                             <li>Type: Rollercoaster</li>
                             <li>Location: Zone3</li>
@@ -220,6 +248,7 @@ export default function HomePage(){
                 <div class="column">
                     <div class="card">
                         <ul>
+                            <li><img class="view" src={CosmicCruiser} alt="Cosmic Cruiser"></img></li>
                             <li><h3>Cosmic Cruiser</h3></li>
                             <li>Type: Rollercoaster</li>
                             <li>Location: Zone3</li>
@@ -230,6 +259,7 @@ export default function HomePage(){
                 <div class="column">
                     <div class="card">
                         <ul>
+                            <li><img class="view" src={Inferno} alt="Inferno"></img></li>
                             <li><h3>Inferno</h3></li>
                             <li>Type: Rollercoaster</li>
                             <li>Location: Zone3</li>
@@ -240,7 +270,8 @@ export default function HomePage(){
                 <div class="column">
                     <div class="card">
                         <ul>
-                            <li><h3>Dragon Lair</h3></li>
+                            <li><img class="view" src={DragonsLair} alt="Dragons Lair"></img></li>
+                            <li><h3>Dragons Lair</h3></li>
                             <li>Type: Rollercoaster</li>
                             <li>Location: Zone3</li>
                             <li>Status: Open</li>
@@ -250,6 +281,7 @@ export default function HomePage(){
                 <div class="column">
                     <div class="card">
                         <ul>
+                            <li><img class="view" src={DinoAdventure} alt="Dino Adventure"></img></li>
                             <li><h3>Dino Adventure</h3></li>
                             <li>Type: Rollercoaster</li>
                             <li>Location: Zone3</li>
