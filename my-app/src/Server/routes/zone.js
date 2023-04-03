875d0d563daa7cc8a9b94d8045072eaa4004c6b9
programwithjohn <john.cox0321@gmail.com>
2023-04-02 22:15:18 -0500

    fixed Concession CSS

--- my-app/src/Server/routes/zone.js
+++ my-app/src/Server/routes/zone.js
@@ -4,10 +4,35 @@ const { getReqData } = require('../helpers/utils');
 const { sendResponse } = require("../helpers/response");
 
 module.exports = {
+    // First Data Report 
     async getZoneIncome(req, res) {
+        /*
+            Date Example:
+            {
+                "zone": "All"
+                "start_date": "DateTime()"
+                "end_date" "DateTime()"
+            }
+        */
         const bodyData = await getReqData(req);
-        const attemptLogin = JSON.parse(bodyData);
+        const zoneData = JSON.parse(bodyData);
 
+        /*
+            We are going to get the type of zone 
+            
+            Zone Options: All, A, B, C, ...
+            Date Option: (None) - All time grand total
+            Date Option: (Date 1 - Date 2) - Grand total between time.
+        */
+
+        
+
+        //If All
+            // If none
+            // If date option
+        //If zone
+            // If none
+            // If date option
         return sendResponse(req, res, 200, "Logged in")
 
     }
