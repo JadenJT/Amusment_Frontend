bf3a35ff2498e0eece0ea2599e1cb8949d162af7
programwithjohn <john.cox0321@gmail.com>
2023-04-13 18:22:50 -0500

    A bunch of routes done, redone

--- my-app/src/Server/routes/ticket.js
+++ my-app/src/Server/routes/ticket.js
@@ -6,19 +6,19 @@ module.exports = {
     /*
         POST Data Example: 
         {
-            "dateTime": Date()
+            "customer_email"
         }
     */
-    async postTicket(req, res) {
+    async buyTicket(req, res) {
 
         const bodyData = await getReqData(req);
         const newTicket = JSON.parse(bodyData);
 
         // const dateTime = new Date(); //This is only for testing purposes! Do NOT deploy to live site. 
-        const queryData = [null, dateTime, newTicket.price, newTicket.ride_id, newTicket.customer_id];
+        const queryData = [null, dateTime, newTicket.price, newTicket.ride_id, newTicket.email];
 
         db.query(
-            'INSERT INTO ticket(ticket_id, date, price, ride_id, customer_id) VALUES (?);', [queryData],
+            'INSERT INTO ticket(ticket_id, date, price, ride_id, email) VALUES (?);', [queryData],
             function(err, result) {
                 if(err) return sendResponse(req, res, 500, `Database error`, err);
                 return sendResponse(req, res, 201, `Ticket added to customer ID: ${newTicket.customer_id}`, newTicket);
@@ -32,7 +32,7 @@ module.exports = {
             "ticket_id": 3
         }
     */
-    async deleteTicket(req, res){
+    async removeTicket(req, res){
         const bodyData = await getReqData(req);
         const newTicket = JSON.parse(bodyData);
         
@@ -46,32 +46,50 @@ module.exports = {
     /*
         POST Data Example: 
         {
-            "customer_id": 8
+            "customer_email": "john.cox1@gmail.com"
         }
     */
-    async getTicketCustomer(req, res){
+    async ticketsOwn(req, res){
         const bodyData = await getReqData(req);
         const customerJSON = JSON.parse(bodyData);
-        const customer_id = customerJSON.customer_id;
+        const customer_email = customerJSON.customer_email;
 
         const [rows, fields] = await db.promise().execute(
             'SELECT * FROM ticket WHERE customer_id = ?', [customer_id])
-        return sendResponse(req, res, 200, `Customer ID: ${customer_id} Ticket fetched`, rows);
+        return sendResponse(req, res, 200, `Customer Tickets Fetched`, rows);
     },
-
     /*
-        POST Data Example: 
+        GET Data Example
         {
-            "ride_id": 11
+            "ride_id": 1,
+            "ride_name": "SkyBlade",
+            "zone": "a",
+            "ride_type": 30,
+            "start_date": "2023-04-01 08:00:00",
+            "end_date": "2023-04-02 12:00:00"
         }
     */
-    async getTicketRides(req, res){
+    async ticketReport (req, res) {
         const bodyData = await getReqData(req);
-        const customerJSON = JSON.parse(bodyData);
-        const ride_id = customerJSON.ride_id;
+        const ticketJSON = JSON.parse(bodyData);
+        const ride_id = ticketJSON.ride_id;
+        const ride_name = ticketJSON.ride_name;
+        const zone = ticketJSON.zone;
+        const ride_type = ticketJSON.ride_type;
+        const start_date = ticketJSON.start_date;
+        const end_date = ticketJSON.end_date;
 
-        const [rows, fields] = await db.promise().execute(
-            'SELECT * FROM ticket WHERE ride_id = ?', [ride_id])
-        return sendResponse(req, res, 200, `Ride ID: ${ride_id} Ticket fetched`, rows);
+        let query = 'SELECT RIDE.name AS Ride_Name, RIDE.zone_id AS Ride_Zone, TIK.price AS Ticket_Amount, TIK.date AS Date_Recorded FROM master.ride as RIDE, master.ticket AS TIK WHERE RIDE.ride_id = TIK.ride_id ';
+
+        if (ride_id != null) query += `AND RIDE.ride_id = ${ride_id} `
+        if (ride_name != null) query += `AND RIDE.name = '${ride_name}' `
+        if (zone != null) query += `AND RIDE.zone_i = '${zone}' `
+        if (ride_type != null) query += `AND TIK.price = ${ride_type} `
+        if (start_date != null && end_date != null) query += `AND TIK.date BETWEEN '${start_date}' AND '${end_date}' ` 
+        
+        query += ';'
+        
+        const [row, fields] = await db.promise().execute(query);
+        return sendResponse(req, res, 200, `Ticket report gathered.`, row)
     }
 }
