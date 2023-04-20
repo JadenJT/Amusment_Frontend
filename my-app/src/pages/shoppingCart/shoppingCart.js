46790045f839a7a3f959223e4a780ffc3b39fc37
JadenJT <Jaden.thai1@gmail.com>
2023-04-19 23:21:10 -0500

    Update shoppingCart.js

--- my-app/src/pages/shoppingCart/shoppingCart.js
+++ my-app/src/pages/shoppingCart/shoppingCart.js
@@ -1,9 +1,31 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../../components/cartContext/CartContext';
 import './shoppingCart.css';
+import { UserContext } from '../../App';
+
 
 function ShoppingCart() {
     const { cartTotal } = useContext(ShopContext);
+
+    const handleOnSubmit = async (event) => {
+        event.preventDefault();
+
+        const personData = {
+            Ticket_amount: 0,
+            date: "",
+            ride_name: "",
+            email: "",
+        };
+
+        const response = await fetch('http://localhost:8080/ticket/buy', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(personData)
+        })
+    }
+
     return (
 
         <div className='cart-cover'>
