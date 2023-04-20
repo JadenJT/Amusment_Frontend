deed61038f94c90b37aa6363aba84165918672bf
MiguelSal <123981962+MiguelSal@users.noreply.github.com>
2023-04-20 00:13:24 -0500

    edit

--- my-app/src/pages/Reports/TicketSalesReport.js
+++ my-app/src/pages/Reports/TicketSalesReport.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Report.css'
 
-import { tempRD } from './tempReportData';
-
 export default function Home(){
     const [ticketValue, setTicketValue] = useState([]);
     const [show, setShow] = useState(false);
