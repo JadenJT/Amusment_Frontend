408ac9953815a6c7056d381f31b5405ac56293b0
John Cox <john.cox0321@gmail.com>
2023-04-23 18:01:52 -0500

    yes

--- my-app/src/Server/routes/giftshop.js
+++ my-app/src/Server/routes/giftshop.js
@@ -56,7 +56,9 @@ module.exports = {
             const selected_giftshop = req.body.selected_giftshop;
             const name = req.body.name;
             const zone_id = req.body.zone_id;
-            const image = req.files[0].buffer.toString('binary');
+            if (req.files[0]) {
+                var image = req.files[0].buffer.toString('binary');
+            }
 
             let query = 'UPDATE master.giftshop SET ';
 
