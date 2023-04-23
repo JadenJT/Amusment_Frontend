78217dd130ebb60ca9ce49fdc540f7dbf489d712
John Cox <john.cox0321@gmail.com>
2023-04-23 18:11:41 -0500

    no

--- my-app/src/Server/routes/giftshop.js
+++ my-app/src/Server/routes/giftshop.js
@@ -87,7 +87,6 @@ module.exports = {
         const upload = multer()
         upload.any()(req, res, async (err) => {
             const name = req.body.name;
-            console.log(name)
             const query = `UPDATE master.giftshop SET perm_closed = TRUE WHERE name = '${name}';`
             const [rows, fields] = await db.promise().execute(query)
             return sendResponse(req, res, 200, "Removed Giftshop")
